538fdf9b12cf40d9f22670d4bed5d817fc059d1f
Korawit63130500001 <korawit.sit@mail.kmutt.ac.th>
2023-10-22 23:36:42 +0700

    [/]edit response

--- api-poc/Server/src/main/java/sit/capstone/apipoc/Controller/MapController.java
+++ api-poc/Server/src/main/java/sit/capstone/apipoc/Controller/MapController.java
@@ -1,12 +1,24 @@
 package sit.capstone.apipoc.Controller;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import sit.capstone.apipoc.DTO.JsonReader;
 import sit.capstone.apipoc.DTO.LatLng;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class MapController {
@@ -15,16 +27,24 @@ public class MapController {
     private String api_key;
 
     @RequestMapping("/api/longdo/")
-    public ResponseEntity<Object> findDistance(@RequestBody LatLng location){
+    public ResponseEntity findDistance(@RequestBody LatLng location) throws IOException {
 
         RestTemplate restTemplate = new RestTemplate();
 
         System.out.println(api_key);
-        Object result = restTemplate.getForObject(
-                "https://api.longdo.com/RouteService/json/route/guide?flon="+location.getFlon()+"&flat="+location.getFlat()+"&tlon="+location.getTlon()+"&tlat="+location.getTlat()+"&mode=t&type=25&locale=th&key="+api_key,Object.class);
+        String result = restTemplate.getForObject(
+                "https://api.longdo.com/RouteService/json/route/guide?flon="+location.getFlon()+"&flat="+location.getFlat()+"&tlon="+location.getTlon()+"&tlat="+location.getTlat()+"&mode=t&type=25&locale=th&key="+api_key, String.class);
 
         System.out.println(result);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(result);
+        float distanceValue = jsonNode.at("/data/0/distance").asInt();
+//        JsonReader meta = objectMapper.readValue(result,JsonReader.class);
+        System.out.println(distanceValue);
+        if(distanceValue<6000){
+            return new ResponseEntity("Success",HttpStatus.OK);
+        }
+        return new ResponseEntity("Event failed",HttpStatus.BAD_REQUEST);
 
     }
 }
---
+++ api-poc/Server/src/main/java/sit/capstone/apipoc/DTO/JsonReader.java
@@ -0,0 +1,14 @@
+package sit.capstone.apipoc.DTO;
+
+import java.util.List;
+import java.util.Map;
+
+public class JsonReader {
+
+        private Map<String, Object> meta;
+        private List<Map<String, Object>> data;
+
+        // Getters and setters
+
+
+}
