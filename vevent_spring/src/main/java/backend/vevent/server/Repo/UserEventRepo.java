d88db061f3fe842b0bbbce7c028d703d2658ff23
korawit <korawit-2545_@hotmail.com>
2023-12-10 18:15:58 +0700

    [+]Update validate status

--- vevent_spring/src/main/java/backend/vevent/server/Repo/UserEventRepo.java
+++ vevent_spring/src/main/java/backend/vevent/server/Repo/UserEventRepo.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserEventRepo extends JpaRepository<UsersEvent,Integer> {
 
     @Query(value = "SELECT * FROM users_events WHERE user_email LIKE :uEmail",nativeQuery = true)
     List<UsersEvent> findAllEventByUEmail(@Param("uEmail")String uEmail);
+
+    @Query(value = "SELECT * FROM users_events WHERE user_email LIKE :uEmail and event_id = :eid",nativeQuery = true)
+    UsersEvent findByEmailAndId(@Param("uEmail")String uEmail,@Param("eid")Integer eid);
 }
