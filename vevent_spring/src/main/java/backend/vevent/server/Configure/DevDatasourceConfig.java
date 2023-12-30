532393e5c31b67915c20d3c53479e2b7db3f4e83
korawit <korawit-2545_@hotmail.com>
2023-12-30 15:56:17 +0700

    [ADD/UPDATE]Pipeline and Datasource Config

---
+++ vevent_spring/src/main/java/backend/vevent/server/Configure/DevDatasourceConfig.java
@@ -0,0 +1,34 @@
+package backend.vevent.server.Configure;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+@Configuration
+@Profile("dev")
+public class DevDatasourceConfig {
+
+    @Value("${spring.datasource.dev.url}")
+    private String devURL;
+    @Value("${spring.datasource.dev.username}")
+    private String devUser;
+    @Value("${spring.datasource.dev.password}")
+    private String devPass;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setUrl(devURL);
+        dataSource.setUsername(devUser);
+        dataSource.setPassword(devPass);
+
+        return dataSource;
+    }
+
+}
