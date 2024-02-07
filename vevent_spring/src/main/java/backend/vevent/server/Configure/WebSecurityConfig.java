9ea0293e45aa91713ef41b7c522def9458ecaf21
Korawit <korawit.sit@mail.kmutt.ac.th>
2024-02-07 16:24:08 +0700

    [EDIT]Allow Validate API

--- vevent_spring/src/main/java/backend/vevent/server/Configure/WebSecurityConfig.java
+++ vevent_spring/src/main/java/backend/vevent/server/Configure/WebSecurityConfig.java
@@ -91,7 +91,7 @@ public class WebSecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/auth","/graphql")
+                        .requestMatchers("/api/auth","/graphql","/api/distance","/api/qrcode")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
